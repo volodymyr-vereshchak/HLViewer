f8a90434d7be349edc27473d1791676fef353972
Volodymyr Vereshchak <volodymyr.vereshchak@gmail.com>
2024-12-28 15:38:51 +0200

    added logs, refactored code, added endpoints for lumg

--- api/endpoints/day_archive_ep.py
+++ api/endpoints/day_archive_ep.py
@@ -3,12 +3,6 @@ from fastapi import APIRouter, status
 from backend.db.dao.daily_archive_dao import DailyArchiveDao
 from backend.db.models import DailyArchiveList
 
-# router = APIRouter()
-#
-# @router.get("/day_archive/", response_model=list[DailyArchiveList])
-# async def get_day_archive():
-#     daily_archives = DailyArchiveDao().get_all()
-#     return daily_archives
 
 class DailyArchiveRouter:
     def __init__(self):
@@ -16,6 +10,7 @@ class DailyArchiveRouter:
         self.router.add_api_route(
             path="/day_archive/",
             endpoint=self.get_day_archive,
+            response_model=list[DailyArchiveList],
             tags=["daily"],
             methods=["GET"],
             status_code=status.HTTP_200_OK
