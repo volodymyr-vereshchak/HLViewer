bb544091618698372ca4bf82bcc11a4ab3f5ec33
Volodymyr Vereshchak <volodymyr.vereshchak@gmail.com>
2025-03-20 20:04:50 +0200

    fix preload_db.py for new sys edit logic

--- backend/db/preload_db/preload_db.py
+++ backend/db/preload_db/preload_db.py
@@ -26,22 +26,17 @@ async def preload_db():
         path = "backend/db/preload_db/FLOWTYPE.json"
         with open(path, "r", encoding="utf8") as file:
             flow_type = json.load(file)["FLOWTYPE"]
-        unique_type_id_calc = set([flow_dict["ID_TYPE"] for flow_dict in flow_type])
         instance_list = [
             {
                 "type_id": flow_dict["ID_TYPE"],
                 "type_name": flow_dict["TYPENAME"].strip(),
-                "updated_at": datetime.now()
+                "updated_at": datetime.now(),
             }
             for flow_dict in flow_type
         ]
         await GasVolumeCalcTypeDao(session=session).bulk_upsert_with_update(
             instance_list, GAS_VOLUME_CALC_TYPE_CONSTRAINT
         )
-        type_id_dict = {
-            type_id: await GasVolumeCalcTypeDao(session=session).get_by_type_id(type_id)
-            for type_id in unique_type_id_calc
-        }
 
         path = "backend/db/preload_db/EDITNAME.json"
         with open(path, "r", encoding="utf8") as file:
@@ -49,7 +44,7 @@ async def preload_db():
         instance_list = [
             {
                 "edit_type_id": flow_dict["EDIT_ID"],
-                "gas_volume_calc_type_id": type_id_dict[flow_dict["ID_TYPE"]],
+                "gas_volume_calc_type_id": flow_dict["ID_TYPE"],
                 "edit_name": flow_dict["EDITNAME"].strip(),
                 "updated_at": datetime.now(),
             }
@@ -65,7 +60,7 @@ async def preload_db():
         instance_list = [
             {
                 "sys_type_id": flow_dict["SYS_ID"],
-                "gas_volume_calc_type_id": type_id_dict[flow_dict["ID_TYPE"]],
+                "gas_volume_calc_type_id": flow_dict["ID_TYPE"],
                 "sys_name": flow_dict["SYSNAME"].strip(),
                 "updated_at": datetime.now(),
             }
@@ -75,7 +70,9 @@ async def preload_db():
 
         for i in range(0, len(instance_list), batch_size):
             batch = instance_list[i : i + batch_size]
-            await SysTypeDao(session=session).bulk_upsert_with_update(batch, SYS_TYPE_CONSTRAINT)
+            await SysTypeDao(session=session).bulk_upsert_with_update(
+                batch, SYS_TYPE_CONSTRAINT
+            )
 
 
 if __name__ == "__main__":
