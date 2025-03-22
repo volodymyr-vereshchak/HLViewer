cd34d71358fa2ef6bad5e906c78fd0b0bffbda0a
Volodymyr Vereshchak <volodymyr.vereshchak@gmail.com>
2025-03-22 16:48:33 +0200

    added validation for coefs

--- backend/db/models/params_model.py
+++ backend/db/models/params_model.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from typing import TYPE_CHECKING
 
+from pydantic import field_validator
 from sqlmodel import Field, UniqueConstraint, Relationship
 
 from .base_model import HlBaseModel
@@ -52,6 +53,18 @@ class Param(ParamBase, table=True):
 class ParamList(ParamBase):
     id: int
     line_id: int
+
+    @field_validator("A0su", "A0pipe")
+    def validate_a0(cls, value: float):
+        return value * 1e6
+
+    @field_validator("A1su", "A1pipe")
+    def validate_a1(cls, value: float):
+        return value * 1e9
+
+    @field_validator("A2su", "A2pipe")
+    def validate_a2(cls, value: float):
+        return value * 1e12
 
 
 class ParamCreate(ParamBase):
