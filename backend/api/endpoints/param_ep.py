59bf737349e3cc22b877a6899456fea273594149
Volodymyr Vereshchak <volodymyr.vereshchak@gmail.com>
2025-03-23 09:49:02 +0200

    added query to base dao lat for date_to

--- backend/api/endpoints/param_ep.py
+++ backend/api/endpoints/param_ep.py
@@ -1,5 +1,10 @@
+from datetime import datetime
+
+from fastapi import Query
+
 from backend.api.endpoints.base_archive_ep import BaseArchiveRouter
 from backend.db.dao.param_dao import ParamDao
+from backend.db.engine import async_session_factory
 from backend.db.models import ParamList
 
 
@@ -11,6 +16,17 @@ class ParamRouter(BaseArchiveRouter):
             tags=["param"],
             archive_dao=ParamDao,
         )
+
+    async def get_archive(
+        self,
+        from_date: datetime = Query(None),
+        to_date: datetime = Query(None),
+        line_id: list[int] = Query(None),
+    ):
+        async with async_session_factory() as session:
+            archive_dao = self.archive_dao(session=session)
+            archives = await archive_dao.get_last_for_to_date(to_date, line_id)
+            return archives
 
 
 param_router = ParamRouter().router
